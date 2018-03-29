1dedf6b830caea4c9a75b0471b9c89ed5c7611a2
Srinivasan <kksrini89@gmail.com>
2018-03-29 21:11:53 +0530

    Initial check in is made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule
